587c52446276b7bdcca87b23be7bc0010bf9dc7d
Duo Lu <duo.lu@mbp13.massive.com.au>
2016-08-31 15:58:45 +1000

    reinitialize the project, now I start from the redux example first

---
+++ actions/actionTypes.js
@@ -0,0 +1,2 @@
+export const ADD_TODO = "ADD_TOTO"
+export const REMOVE_TODO = "REMOVE_TOTO"
---
+++ actions/index.js
@@ -0,0 +1,7 @@
+import {ADD_TODO, REMOVE_TODO} from "./actionTypes"
+
+{
+  type: ADD_TODO,
+  text: "Build my first redux app"
+}
+
